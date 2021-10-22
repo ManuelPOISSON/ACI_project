6dfd3fb6b32a642b1f4f3151273f7a4fd11eabaa
Volodia PAROL-GUARINO <volodia.parolguarino@protonmail.com>
2021-10-22 10:37:16 +0200

    Working on pulsar

---
+++ deploy/pulsar/test/pub.py
@@ -0,0 +1,27 @@
+import pulsar
+
+client = pulsar.Client('pulsar://broker-beijing:6650')
+
+producer = client.create_producer('persistent://my-tenant/my-namespace/my-topic')
+# producer = client.create_producer('my-topic')
+
+for i in range(10):
+    producer.send(('Hello-%d' % i).encode('utf-8'))
+
+client.close()
+
+
+# from pulsar import Function
+
+# # Example function that uses the built in publish function in the context
+# # to publish to a desired topic based on config
+# class PublishFunction(Function):
+#   def __init__(self):
+#     pass
+
+#   def process(self, input, context):
+#     publish_topic = "publishtopic"
+#     if "publish-topic" in context.get_user_config_map():
+#       publish_topic = context.get_user_config_value("publish-topic")
+#     context.publish(publish_topic, input + '!')
+#     return
---
+++ deploy/pulsar/test/sub.py
@@ -0,0 +1,17 @@
+import pulsar
+
+client = pulsar.Client('pulsar://broker-beijing:6650')
+
+consumer = client.subscribe('my-tenant/my-namespace/my-topic', 'my-subscription')
+
+while True:
+    msg = consumer.receive()
+    try:
+        print("Received message '{}' id='{}'".format(msg.data(), msg.message_id()))
+        # Acknowledge successful processing of the message
+        consumer.acknowledge(msg)
+    except:
+        # Message failed to be processed
+        consumer.negative_acknowledge(msg)
+
+client.close()
