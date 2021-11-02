3e811f5dbd97ca048b1e1b1972f3c82e3272946a
PitouGames <pitou.games@gmail.com>
2021-11-02 11:00:28 +0100

    [Backend] Add CORS

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 import datetime
 import db_connector
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
